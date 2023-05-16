97ce5ffded20b0990410787dcda044f08e792083
jojo155_m <joanapereira2109@gmail.com>
2023-05-15 21:27:33 -0300

    Create numeros [].py
    teste base

---
+++ numeros [].py
@@ -0,0 +1,25 @@
+numeros []
+
+
+while True:
+    num = int(input("digitar um valor positivo -1 para parar "))
+
+    if num ==-1:
+        break
+
+    if num > 0:
+        numeros.append (num)
+
+if len (numeros) == 0 :
+    print ("nenhum número positivo foi digitado")
+
+else:
+    soma = num(numeros)
+    media= soma /len (numeros)
+    menor = min(numeros)
+    maior = max (numeros )  
+
+    print("soma dos numeros :", soma)
+    print("medía dos numeros :",media)
+    print("menos numeros ", menor)
+    print("maior numeros ", maior )      
